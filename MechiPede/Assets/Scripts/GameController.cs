f121916f0ac826ca69d6e235926a4b247d134ddc
WTnis <skellgun@gmx.de>
2021-11-26 11:00:00 +0100

    New things i´ve done
    All new things that are in the game

--- MechiPede/Assets/Scripts/GameController.cs
+++ MechiPede/Assets/Scripts/GameController.cs
@@ -8,7 +8,7 @@ public class GameController : MonoBehaviour
     public float masterSpeed;                   //Gives Speed to Pede
     public Scene checkScene;                    //Checks Scene
 
-    [SerializeField] public int maxLevel = 30;    //Max amount of levels
+    [SerializeField] public int maxLevel = 10;    //Max amount of levels
 
 
 
@@ -38,6 +38,46 @@ public class GameController : MonoBehaviour
         if (scnName == "Level2")
         {
             masterSpeed = 5f;
+        }
+
+        if (scnName == "Level3")
+        {
+            masterSpeed = 7f;
+        }
+
+        if (scnName == "Level4")
+        {
+            masterSpeed = 10f;
+        }
+
+        if (scnName == "Level5")
+        {
+            masterSpeed = 12f;
+        }
+
+        if (scnName == "Level6")
+        {
+            masterSpeed = 14f;
+        }
+
+        if (scnName == "Level7")
+        {
+            masterSpeed = 16f;
+        }
+
+        if (scnName == "Level8")
+        {
+            masterSpeed = 18f;
+        }
+
+        if (scnName == "Level9")
+        {
+            masterSpeed = 20f;
+        }
+
+        if (scnName == "Level10")
+        {
+            masterSpeed = 25f;
         }
     }
 }
